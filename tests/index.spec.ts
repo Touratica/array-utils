ba78c73aaa5089107858a383dfc48a37f070b03c
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2023-01-05 07:32:53 +0000

    feat: Add Array.deepCopy, Array.shuffle and Array.prototype.shuffle (#1)
    * Add .gitignore
    
    Adds .gitignore based on template Node.gitignore
    
    * Configure project
    
    * Add test suite
    
    * Update .gitignore
    
    Include output of tsc (lib directory)
    
    * Add code for Array.deepCopy, Array.shuffle and Array.prototype.shuffle

---
+++ tests/index.spec.ts
@@ -0,0 +1,45 @@
+import "mocha";
+import { assert } from "chai";
+
+import "../src/index";
+
+describe("Array", () => {
+  describe("#deepCopy", () => {
+    it("should return a deep copy of the array", () => {
+      let arr = [2, 11, 37, 42];
+      let a2 = Array.deepCopy(arr);
+      assert.deepEqual(arr, a2);
+      assert.notEqual(arr, a2);
+    });
+
+    it("should return a deep copy of the array with nested arrays", () => {
+      let arr = [2, 11, [37, 42]];
+      let a2 = Array.deepCopy(arr);
+      assert.deepStrictEqual(arr, a2);
+      assert.notEqual(arr, a2);
+    });
+  });
+  describe("#shuffle", () => {
+    it("should return a shuffled array", () => {
+      let arr = [2, 11, 37, 42];
+      let a2 = Array.shuffle(arr);
+      assert.notEqual(arr, a2);
+      assert.sameDeepMembers(arr, a2);
+    });
+
+    it("should return a shuffled array with nested arrays", () => {
+      let arr = [2, 11, [37, 42]];
+      let a2 = Array.shuffle(arr);
+      assert.notEqual(arr, a2);
+      assert.sameDeepMembers(arr, a2);
+    });
+  });
+
+  describe("#prototype.shuffle", () => {
+    it("should return the same array shuffled", () => {
+      let arr = [2, 11, 37, 42];
+      let a2 = arr.shuffle();
+      assert.deepEqual(arr, a2);
+    });
+  });
+});
