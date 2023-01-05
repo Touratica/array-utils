268ea5f6a528a3fa94bbbff85692861076a0ec2a
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2023-01-05 09:46:01 +0000

    fix(docs): fix documentation

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,73 @@
 interface ArrayConstructor {
-  deepCopy<T>(arr: Array<T>): Array<T>;
-  shuffle<T>(arr: Array<T>): Array<T>;
+  /**
+   * Deep copies an array.
+   * @param {T[]} array The array to deep copy.
+   * @returns {T[]} A deep copy of the array.
+   * @example
+   * const arr = [1, 2, 3];
+   * const arrCopy = Array.deepCopy(arr);
+   * console.log(arrCopy); // [1, 2, 3]
+   * console.log(arrCopy === arr); // false
+   * console.log(arrCopy[0] === arr[0]); // true
+   * console.log(arrCopy[1] === arr[1]); // true
+   * console.log(arrCopy[2] === arr[2]); // true
+   * @example
+   * const arr = [{ a: 1 }, { b: 2 }, { c: 3 }];
+   * const arrCopy = Array.deepCopy(arr);
+   * console.log(arrCopy); // [{ a: 1 }, { b: 2 }, { c: 3 }]
+   * console.log(arrCopy === arr); // false
+   * console.log(arrCopy[0] === arr[0]); // false
+   * console.log(arrCopy[1] === arr[1]); // false
+   * console.log(arrCopy[2] === arr[2]); // false
+   * console.log(arrCopy[0].a === arr[0].a); // true
+   * console.log(arrCopy[1].b === arr[1].b); // true
+   * console.log(arrCopy[2].c === arr[2].c); // true
+   * @example
+   * const arr = [[1, 2], 4, 5];
+   * const arrCopy = Array.deepCopy(arr);
+   * console.log(arrCopy); // [[1, 2], 4, 5]
+   * console.log(arrCopy === arr); // false
+   * console.log(arrCopy[0] === arr[0]); // false
+   * console.log(arrCopy[0][0] === arr[0][0]); // true
+   * console.log(arrCopy[0][1] === arr[0][1]); // true
+   * console.log(arrCopy[0][2] === arr[0][2]); // true
+   */
+  deepCopy<T>(array: Array<T>): Array<T>;
+
+  /**
+   * Shuffles an array, whitout mutating the original array.
+   * @param {T[]} array The array to shuffle.
+   * @returns {T[]} The shuffled array.
+   * @example
+   * const arr = [1, 2, 3];
+   * const shuffledArr = Array.shuffle(arr);
+   * console.log(shuffledArr); // [2, 3, 1]
+   * console.log(shuffledArr === arr); // false
+   */
+  shuffle<T>(array: Array<T>): Array<T>;
 }
 
 interface Array<T> {
+  /**
+   * Shuffles array in place, using Fisher-Yates algorithm.
+   * @returns {[]} The shuffled array.
+   * @example
+   * const arr = [1, 2, 3];
+   * const shuffledArr = arr.shuffle();
+   * console.log(shuffledArr); // [2, 3, 1]
+   * console.log(shuffledArr === arr); // true
+   */
   shuffle(): Array<T>;
 }
 
-/**
- * Deep copies an array.
- * @param {T[]} array The array to deep copy.
- * @returns {T[]} A deep copy of the array.
- * @example
- * const arr = [1, 2, 3];
- * const arrCopy = Array.deepCopy(arr);
- * console.log(arrCopy); // [1, 2, 3]
- * console.log(arrCopy === arr); // false
- * console.log(arrCopy[0] === arr[0]); // true
- * console.log(arrCopy[1] === arr[1]); // true
- * console.log(arrCopy[2] === arr[2]); // true
- * @example
- * const arr = [{ a: 1 }, { b: 2 }, { c: 3 }];
- * const arrCopy = Array.deepCopy(arr);
- * console.log(arrCopy); // [{ a: 1 }, { b: 2 }, { c: 3 }]
- * console.log(arrCopy === arr); // false
- * console.log(arrCopy[0] === arr[0]); // false
- * console.log(arrCopy[1] === arr[1]); // false
- * console.log(arrCopy[2] === arr[2]); // false
- * console.log(arrCopy[0].a === arr[0].a); // true
- * console.log(arrCopy[1].b === arr[1].b); // true
- * console.log(arrCopy[2].c === arr[2].c); // true
- * @example
- * const arr = [[1, 2], 4, 5];
- * const arrCopy = Array.deepCopy(arr);
- * console.log(arrCopy); // [[1, 2], 4, 5]
- * console.log(arrCopy === arr); // false
- * console.log(arrCopy[0] === arr[0]); // false
- * console.log(arrCopy[0][0] === arr[0][0]); // true
- * console.log(arrCopy[0][1] === arr[0][1]); // true
- * console.log(arrCopy[0][2] === arr[0][2]); // true
- */
-Array.deepCopy = function <T>(array: Array<T>): T[] {
-  return JSON.parse(JSON.stringify(array));
+Array.deepCopy = function <T>(arr: Array<T>): T[] {
+  return JSON.parse(JSON.stringify(arr));
 };
 
-/**
- * Shuffles array in place, using Fisher-Yates algorithm.
- * @returns {[]} The shuffled array.
- * @example
- * const arr = [1, 2, 3];
- * const shuffledArr = arr.shuffle();
- * console.log(shuffledArr); // [2, 3, 1]
- * console.log(shuffledArr === arr); // true
- */
+Array.shuffle = function <T>(arr: Array<T>): T[] {
+  return Array.deepCopy(arr).shuffle();
+};
+
 Array.prototype.shuffle = function <T>(): T[] {
   let currentIndex: number = this.length,
     randomIndex: number;
@@ -71,18 +86,4 @@ Array.prototype.shuffle = function <T>(): T[] {
   }
 
   return this;
-};
-
-/**
- * Shuffles an array, whitout mutating the original array.
- * @param {T[]} array The array to shuffle.
- * @returns {T[]} The shuffled array.
- * @example
- * const arr = [1, 2, 3];
- * const shuffledArr = Array.shuffle(arr);
- * console.log(shuffledArr); // [2, 3, 1]
- * console.log(shuffledArr === arr); // false
- */
-Array.shuffle = function <T>(array: Array<T>): T[] {
-  return Array.deepCopy(array).shuffle();
 };
